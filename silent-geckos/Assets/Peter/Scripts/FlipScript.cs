8fa35de647b898c199ed5952b04944cf0fd48ee9
Peter Newman <newmanp5@coventry.ac.uk>
2022-01-28 22:38:54 +0000

    flip script

--- silent-geckos/Assets/Peter/Scripts/FlipScript.cs
+++ silent-geckos/Assets/Peter/Scripts/FlipScript.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FlipScript : MonoBehaviour
 {
+    public GameObject player;
+    public bool isFlipped;
+    public event Action<bool> OnPhaseChange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("j"))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        isFlipped = !isFlipped;
+        OnPhaseChange?.Invoke(!isFlipped);
+        Vector3 position = new Vector3(0, player.transform.position.y * -2,0);
+        player.transform.Translate(position);
+        player.GetComponent<Rigidbody2D>().velocity = new Vector3(player.GetComponent<Rigidbody2D>().velocity.x,player.GetComponent<Rigidbody2D>().velocity.y*-1,0);
+        player.GetComponent<Rigidbody2D>().gravityScale *= -1;
     }
 }

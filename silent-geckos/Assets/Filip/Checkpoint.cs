f56859f11dc734ffb2a462ab1effccf2f720f110
Filip Nejedly <nejedlyf@coventry.ac.uk>
2022-01-29 14:41:20 +0000

    Checkpoint Stuff

---
+++ silent-geckos/Assets/Filip/Checkpoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] private CheckpointData checkPointdata;
+    [SerializeField] private ScoreDataSO scoreDataSo;
+    public int checkpointnumber;
+    public void Awake()
+    {
+        if (checkpointnumber == 1)
+        {
+            if (checkPointdata.checkpoint1 == true)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (checkpointnumber == 2)
+        {
+            if (checkPointdata.checkpoint2 == true)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (checkpointnumber == 3)
+        {
+            if (checkPointdata.checkpoint3 == true)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            checkPointdata.SaveLocation(this.transform.position);
+            checkPointdata.MergeData(scoreDataSo);
+            checkPointdata.SaveCheckpoint(checkpointnumber);
+            DontDestroyOnLoad(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ silent-geckos/Assets/Stuart/Scripts/State/CheckpointData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CheckpointData", menuName = "Stuart/CheckpointData")]
+public class CheckpointData : ScoreDataSO
+{
+    public Vector2 position;
+    public bool checkpoint1 = false;
+    public bool checkpoint2 = false;
+    public bool checkpoint3 = false;
+
+
+
+
+    public void MergeData(ScoreDataSO scoreDataSO)
+    {
+        heavenCoins = scoreDataSO.heavenCoins;
+        hellCoins = scoreDataSO.hellCoins;
+        progressBar = scoreDataSO.progressBar;
+    }
+    public void SaveLocation(Vector2 checkPosition)
+    {
+        position = checkPosition;
+    }
+    public void SaveCheckpoint(int checkpoint)
+    {
+        if (checkpoint == 1)
+        {
+            checkpoint1 = true;
+        }
+        if (checkpoint == 2)
+        {
+            checkpoint2 = true;
+        }
+        if (checkpoint == 3)
+        {
+            checkpoint3 = true;
+        }
+    }
+}

1e343e565b2332283bc94ba12dbf43df8fc5b241
Filip Nejedly (nejedlyf) <nejedlyf@coventry.ac.uk>
2022-01-29 18:06:52 +0000

    Checkpoint

--- silent-geckos/Assets/MainBranch/Assets/Scripts/Respawn.cs
+++ silent-geckos/Assets/MainBranch/Assets/Scripts/Respawn.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Respawn : MonoBehaviour
 {
     private GameObject player;
     [SerializeField] private CheckpointData checkPointdata;
     [SerializeField] private ScoreDataSO scoreDataSo;
+    public TextMeshProUGUI goodCounter;
+    public TextMeshProUGUI evilCounter;
+    private int goodScore = 0;
+    private int evilScore = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +20,32 @@ public class Respawn : MonoBehaviour
         {
             player.transform.position = checkPointdata.position;
             checkPointdata.Respawn(scoreDataSo);
+            Debug.Log("3");
+            goodScore += scoreDataSo.heavenCoins;
+            goodCounter.text = goodScore.ToString();
+            evilScore += scoreDataSo.hellCoins;
+            evilCounter.text = evilScore.ToString();
         }
         else if (checkPointdata.checkpoint2 == true)
         {
-            
+            player.transform.position = checkPointdata.position;
+            checkPointdata.Respawn(scoreDataSo);
+            Debug.Log("2");
+            goodScore += scoreDataSo.heavenCoins;
+            goodCounter.text = goodScore.ToString();
+            evilScore += scoreDataSo.hellCoins;
+            evilCounter.text = evilScore.ToString();
         }
 
         else if (checkPointdata.checkpoint1 == true)
         {
-            
+            player.transform.position = checkPointdata.position;
+            checkPointdata.Respawn(scoreDataSo);
+            Debug.Log("1");
+            goodScore += scoreDataSo.heavenCoins;
+            goodCounter.text = goodScore.ToString();
+            evilScore += scoreDataSo.hellCoins;
+            evilCounter.text = evilScore.ToString();
         }
     }
 }
